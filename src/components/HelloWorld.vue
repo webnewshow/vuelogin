d6d330da95cf8c76d364ca92285663fd5d1614fc
webnewshow <53456127+webnewshow@users.noreply.github.com>
2019-11-15 23:06:52 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
       <div class="title">LOGIN</div>
       <div class="user"><input type="text" name="user" placeholder="User"/></div>
       <div class="password"><input type="password" name="password" placeholder="Password"/></div>
+      <div class="sign">Sign in</div>
     </div>
   </div>
 </template>
@@ -17,22 +18,22 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style >
 body{
-  background-color: #353b48;
+  background: #2980b9;
 }
 .login{
   width: 500px;
-  height: 700px;
+  height: 755px;
   margin: 0 auto;
   transform: perspective(800px) translate3d(0,20vh,0) rotate3d(2,0,0,70deg);
-  border-left: 10px solid #f0932b;
-  border-right: 10px solid #f0932b;
-  background-color: #2f3542;
+  border-left: 10px solid white;
+  border-right: 10px solid white;
+  background-color: #3d3d3d;
 }
 .login .title{
   font-weight: bold;
   font-size: 100px;
   text-align: center;
-  color:black;
+  color:#d35400;
   padding-top: 70px;
 }
 .login .user{
@@ -44,17 +45,25 @@ body{
   margin-top: 80px;
 }
 input[name="user"]{
-  border: 2px solid  #ff6b81;
+  border: 3px solid  #d35400;
   outline-style: none;
   width: 350px;
   height: 70px;
   text-align: center;
 }
 input[name="password"]{
-  border: 2px solid  #ff6b81;
+  border: 2px solid  #d35400;
   outline-style: none;
   width: 360px;
   height: 70px;
   text-align: center;
+}
+.sign{
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 80px;
+  font-size: 100px;
+  cursor: pointer;
+  color:#d35400;
 }
 </style>
